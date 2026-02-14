f177572f9bf9ecb9a42d6213a1f1e39f71749514
RafaelAlmeida00 <rafaelalmeid00@gmail.com>
2026-02-14 02:25:21 -0300

    fix: change strategy for input env for test workflow

---
+++ jest.setup.js
@@ -0,0 +1,4 @@
+process.env.NODE_ENV = 'test';
+process.env.DATABASE_URL = 'postgresql://test:test@localhost:5432/test';
+process.env.JWT_SECRET = 'test-secret-key-for-unit-tests';
+process.env.PORT = '3000';
